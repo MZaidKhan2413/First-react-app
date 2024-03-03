0d5ba49224d6127aa66fbf628c4166e9baf07479
Mohammad Zaid Khan <mzaidk222@gmail.com>
2024-03-03 11:03:44 +0530

    Basic changes

--- First Project/src/App.jsx
+++ First Project/src/App.jsx
@@ -6,7 +6,11 @@ function App() {
   return (
     <>
       <h1>This is my first react app</h1>
-      <Card />
+      <div className="container">
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </>
   )
 }
